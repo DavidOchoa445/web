c84c2c967aa7b898299a35fd80087f2b07e516cd
DavidOchoa445 <david8a4455v@gmail.com>
2024-11-11 22:07:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const welcomeMessage = document.getElementById('welcomeMessage');
+    
+    
+    setTimeout(() => {
+      welcomeMessage.style.opacity = 1;
+    }, 500);
+  
+   
+    setTimeout(() => {
+      welcomeMessage.style.opacity = 0;
+    }, 5000);
+  });
+  
